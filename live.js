81c27881d345abf55535b80348d51faddd08ecf4
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2020-11-17 12:59:41 +0200

    Update live.js

--- live.js
+++ live.js
@@ -1,7 +1,12 @@
 var toggle = document.querySelector('.toggle'),
     codedit = document.querySelector('cd'),
+    html = document.querySelector('cd textarea'),
     iframe = document.querySelector('iframe');
 
 toggle.onclick = () => {
   codedit.classList.toggle('open');
+}
+
+function updateIframe() {
+  iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(html.value);
 }
