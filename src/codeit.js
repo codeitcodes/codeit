0603d597ed0e86cf0a672633726e56903ef8b36b
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-06-29 18:31:34 +0300

    Update codeit.js

--- src/codeit.js
+++ src/codeit.js
@@ -44,7 +44,7 @@ codeits.forEach(codeit => {
 
       } else if (mutation.type === 'childList') { // if mutation was a innerHTML mutation
         
-        if (removedNodes.length > 0) {
+        if (removedNodes) {
         
           // if the text didn't change, stop
           if (mutation.removedNodes[0].textContent === mutation.addedNodes[0].textContent) {
