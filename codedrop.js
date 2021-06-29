3a8b5eed376e7713fea8b579bbb48295393d640a
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-06-29 14:34:20 +0300

    Create codedrop.js

---
+++ codedrop.js
@@ -0,0 +1,39 @@
+let codedit = document.querySelector('cd');
+
+codedit.addEventListener('drop', (ev) => {
+  
+  // Prevent default behavior (Prevent file from being opened)
+  ev.preventDefault();
+
+  if (ev.dataTransfer.items) {
+    // Use DataTransferItemList interface to access the file(s)
+    for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+      // If dropped items aren't files, reject them
+      if (ev.dataTransfer.items[i].kind === 'file') {
+        var file = ev.dataTransfer.items[i].getAsFile();
+        console.log('... file[' + i + '].name = ' + file.name);
+      }
+    }
+  } else {
+    // Use DataTransfer interface to access the file(s)
+    for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+      console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+    }
+  }
+  
+})
+
+codedit.addEventListener('dragover', (ev) => {
+  
+  document.body.classList.add('focus');
+  
+  // Prevent default behavior (Prevent file from being opened)
+  ev.preventDefault();
+  
+})
+
+codedit.addEventListener('dragleave', () => {
+  
+  document.body.classList.remove('focus');
+  
+})
