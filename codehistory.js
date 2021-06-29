f2c78123b5d77ce457111e31132830a27f699578
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-06-29 15:44:54 +0300

    Create codehistory.js

---
+++ codehistory.js
@@ -0,0 +1,18 @@
+let codedit = document.querySelector('cd');
+let history = document.querySelector('.history-wrapper');
+
+history.addEventListener('click', () => {
+  
+  if (localStorage.getItem('history')) {
+    // if code in storage, show it
+    codedit.setValue(localStorage.getItem('code'));
+  }
+  
+}
+
+window.onbeforeunload = function() {
+  
+  // set new localStorage value
+  localStorage.setItem('code', codedit.input.value);
+  
+};
