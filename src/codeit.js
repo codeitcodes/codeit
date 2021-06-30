162e044326ebbc6491d0292d0a5730f635ac54d4
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-06-30 20:50:21 +0300

    Update codeit.js

--- src/codeit.js
+++ src/codeit.js
@@ -126,7 +126,7 @@ codeits.forEach(cd => {
       if (overallLength < caretPosInText) {
         lastNode = node;
         if (node.nodeType == 3) {
-          if (node.nodeValue !== ' ') {
+          if (node.nodeValue !== ' ' || node.nodeValue !== '\n') {
 
             overallLength += node.length;
           } else {
