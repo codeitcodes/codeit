a9d8567a062dfbee19142ce092b32e69d05cc467
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-07-05 17:44:17 +0300

    Update codeit.js

--- src/codeit.js
+++ src/codeit.js
@@ -11,55 +11,55 @@
    
 */
 
-//(() => {
+(() => {
   
   // add codeit CSS to head
-  var css = 'cd{display:inline-flex;position:relative;font-family:monospace;text-rendering:optimizeLegibility;font-feature-settings:"kern";background:#f1f3f4;color:#333;border-radius:5px;padding:5px}cd textarea{background:0 0;color:transparent;position:absolute;top:0;left:0;border:0;resize:none;font:inherit;letter-spacing:inherit;line-height:inherit;outline:0;caret-color:#000;white-space:pre;overflow:hidden;padding:inherit;box-sizing:border-box;z-index:1}cd pre{margin:0;font:inherit;letter-spacing:inherit;line-height:inherit}cd code{color:#333}.hljs-comment,.hljs-meta{color:#969896}.hljs-emphasis,.hljs-quote,.hljs-strong,.hljs-template-variable,.hljs-variable{color:#df5000}.hljs-keyword,.hljs-selector-tag,.hljs-type{color:#d73a49}.hljs-attribute,.hljs-bullet,.hljs-literal,.hljs-symbol{color:#0086b3}.hljs-name,.hljs-section{color:#63a35c}.hljs-tag{color:#333}.hljs-attr,.hljs-selector-attr,.hljs-selector-class,.hljs-selector-id,.hljs-selector-pseudo,.hljs-title{color:#6f42c1}.hljs-addition{color:#55a532;background-color:#eaffea}.hljs-deletion{color:#bd2c00;background-color:#ffecec}.hljs-link{text-decoration:underline}.hljs-number{color:#005cc5}.hljs-string{color:#032f62}',
-      head = document.head,
-      style = document.createElement('style');
+  const css = 'cd{display:inline-flex;position:relative;font-family:monospace;text-rendering:optimizeLegibility;font-feature-settings:"kern";background:#f1f3f4;color:#333;border-radius:5px;padding:5px}cd textarea{background:0 0;color:transparent;position:absolute;top:0;left:0;border:0;resize:none;font:inherit;letter-spacing:inherit;line-height:inherit;outline:0;caret-color:#000;white-space:pre;overflow:hidden;padding:inherit;box-sizing:border-box;z-index:1}cd pre{margin:0;font:inherit;letter-spacing:inherit;line-height:inherit}cd code{color:#333}.hljs-comment,.hljs-meta{color:#969896}.hljs-emphasis,.hljs-quote,.hljs-strong,.hljs-template-variable,.hljs-variable{color:#df5000}.hljs-keyword,.hljs-selector-tag,.hljs-type{color:#d73a49}.hljs-attribute,.hljs-bullet,.hljs-literal,.hljs-symbol{color:#0086b3}.hljs-name,.hljs-section{color:#63a35c}.hljs-tag{color:#333}.hljs-attr,.hljs-selector-attr,.hljs-selector-class,.hljs-selector-id,.hljs-selector-pseudo,.hljs-title{color:#6f42c1}.hljs-addition{color:#55a532;background-color:#eaffea}.hljs-deletion{color:#bd2c00;background-color:#ffecec}.hljs-link{text-decoration:underline}.hljs-number{color:#005cc5}.hljs-string{color:#032f62}',
+        head = document.head,
+        style = document.createElement('style');
   head.appendChild(style);
   style.appendChild(document.createTextNode(css));
 
   // get all Codeits
-  var codeits = document.querySelectorAll('cd');
+  const codeits = document.querySelectorAll('cd');
 
   codeits.forEach(cd => {
 
     // create codeit elements
-    var input = document.createElement('textarea');
+    cd.textarea = document.createElement('textarea');
 
-    var pre = document.createElement('pre');
-    var fake = document.createElement('code');
+    cd.pre = document.createElement('pre');
+    cd.code = document.createElement('code');
 
     // style codeit textarea
-    input.setAttribute('spellcheck', 'false');
-    input.setAttribute('rows', 1);
+    cd.textarea.setAttribute('spellcheck', 'false');
+    cd.textarea.setAttribute('rows', 1);
 
     // highlight with specified lang
-    fake.classList = cd.getAttribute('lang') ? cd.getAttribute('lang') : 'hljs';
+    cd.code.classList = cd.getAttribute('lang') ? cd.getAttribute('lang') : 'hljs';
 
     // parse code
-    cd.parsedCode = decodeHTML(cd.innerHTML).replace(/^\n|\n$/g, '');
+    cd.parsedCode = cd.innerText.replace(/^\n|\n$/g, '');
 
     // clear codeit
     cd.innerHTML = '';
 
     // append codeit elements to DOM
-    input = cd.appendChild(input);
-    pre = cd.appendChild(pre);
-    fake = pre.appendChild(fake);
+    cd.appendChild(cd.textarea);
+    cd.appendChild(cd.pre);
+    cd.pre.appendChild(cd.code);
 
     // set codeit textarea value to code
-    input.value = cd.parsedCode;
+    cd.textarea.value = cd.parsedCode;
 
     // if codeit is uneditable, hide input
     if (cd.getAttribute('editable') == 'false') {
-      input.style.display = 'none';
+      cd.textarea.style.display = 'none';
     }
 
     // init codeit behavior
     new Behave({
-      textarea: input,
+      textarea: cd.textarea,
       replaceTab: true,
       softTabs: true,
       tabSize: 2,
@@ -73,62 +73,35 @@
 
     cd.setValue = (code) => {
 
-      input.value = code;
-      update();
+      cd.textarea.value = code;
+      cd.update();
 
     }
 
-    function update() {
+    cd.update = () => {
 
-      fake.innerHTML = escapeHTML(input.value);
+      cd.code.innerText = cd.textarea.value;
 
-      input.style.width = cd.scrollWidth + 'px';
-      input.style.height = cd.scrollHeight + 'px';
+      cd.textarea.style.width = cd.scrollWidth + 'px';
+      cd.textarea.style.height = cd.scrollHeight + 'px';
 
       // if codeit lang not specified, autodetect code lang
       if (!cd.getAttribute('lang')) {
-        fake.classList = 'hljs';
+        cd.code.classList = 'hljs';
       }
 
-      hljs.highlightElement(fake);
+      hljs.highlightElement(cd.code);
 
     }
-    
-   // update textarea
-   var observe;
-   if (window.attachEvent) {
-     observe = (event, handler) => {
-       input.attachEvent('on' + event, handler);
-     };
-   } else {
-     observe = (event, handler) => {
-       input.addEventListener(event, handler, false);
-     };
-   }
-    
-   observe('input', update);
-   observe('cut', update);
-   observe('paste', update);
-   observe('drop', update);
-   observe('keydown', update);
 
-    update();
+    cd.textarea.addEventListener('input', cd.update);
+    cd.textarea.addEventListener('cut', cd.update);
+    cd.textarea.addEventListener('paste', cd.update);
+    cd.textarea.addEventListener('drop', cd.update);
+    cd.textarea.addEventListener('keydown', cd.update);
+
+    cd.update();
 
   });
-  
-  function escapeHTML(unsafe) {
-    return unsafe
-      .replace(/&/g, "&amp;")
-      .replace(/</g, "&lt;")
-      .replace(/>/g, "&gt;")
-      .replace(/"/g, "&quot;")
-      .replace(/'/g, "&#039;");
-  }
-  
-  function decodeHTML(input){
-    var e = document.createElement('textarea');
-    e.innerHTML = input;
-    return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
-  }
-  
-//})();
+
+})();
