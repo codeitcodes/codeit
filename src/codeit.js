3fb0f9856b14a6fa28551e3e6e75f7885e21e3a5
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-07-05 18:19:11 +0300

    Update codeit.js

--- src/codeit.js
+++ src/codeit.js
@@ -83,6 +83,7 @@
       cd.code.innerHTML = escapeHTML(cd.textarea.value);
       
       cd.textarea.style.width = 0;
+      cd.textarea.style.height = 0;
       
       cd.textarea.style.width = cd.scrollWidth + 'px';
       cd.textarea.style.height = cd.scrollHeight + 'px';
