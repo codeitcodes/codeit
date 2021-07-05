3c05069b0e57db9a8661024266b71aa70b0b71ab
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-07-05 15:10:09 +0300

    Update codeit.js

--- src/codeit.js
+++ src/codeit.js
@@ -73,8 +73,8 @@ codeits.forEach(codeit => {
     
     fake.innerHTML = escapeHTML(input.value);
     
-    input.style.width = codeit.clientWidth + 'px';
-    input.style.height = codeit.clientHeight + 'px';
+    input.style.width = codeit.clientWidth - 10 + 'px';
+    input.style.height = codeit.clientHeight - 10 + 'px';
     
     // If lang not specified, clear class for autodetect
     if (!codeit.getAttribute('lang')) {
