2db6612da34bb795895310dea55204faa38194d4
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-07-07 12:49:57 +0300

    Update codegit.js

--- codegit.js
+++ codegit.js
@@ -132,7 +132,18 @@ window.onload = () => {
     cd.textarea.setSelectionRange(getStorage('caret'), getStorage('caret'));
     cd.textarea.focus();
     
-    if (getStorage('sidebar') == 'true') body.classList.add('expanded');
+    if (getStorage('sidebar') == 'true') {
+      
+      body.classList.add('transitioning');
+      body.classList.add('expanded');
+      
+      window.setTimeout(() => {
+        
+        body.classList.remove('transitioning');
+        
+      }, 400);
+      
+    }
     
   }
   
