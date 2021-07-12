e40f4f7d79f0c3dc93fdce9015a81b7c6830d628
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-07-12 11:49:06 +0300

    Create codesearch.js

---
+++ codesearch.js
@@ -0,0 +1,89 @@
+
+// open search screen on click of button
+searchButton.addEventListener('click', () => {
+  
+  header.classList.add('searching');
+  
+  // focus search input
+  searchInput.focus();
+  
+})
+
+// close search screen on click of button
+searchBack.addEventListener('click', () => {
+    
+  // show all files
+  let files = fileWrapper.querySelectorAll('.item[style="display: none;"]');
+  files.forEach(file => { file.style.display = '' });
+  
+  // hide clear button
+  searchClear.classList.remove('visible');
+  
+  header.classList.remove('searching');
+  
+  // clear search input
+  window.setTimeout(() => {
+    
+    searchInput.innerText = '';
+    
+  }, 180);
+  
+})
+
+// search when typed in input
+searchInput.addEventListener('input', () => {
+  
+  let query = searchInput.innerText.toLowerCase();
+  let files = fileWrapper.querySelectorAll('.item');
+  
+  // search files
+  files.forEach(file => {
+    
+    let name = file.querySelector('.name').innerText;
+    
+    if (!name.toLowerCase().includes(query)) {
+
+      file.style.display = 'none';
+
+    }
+
+    else {
+
+      file.style.display = '';
+
+    }
+
+  })
+  
+  // if search query exists
+  if (searchInput.innerText != '') {
+    
+    // show clear button
+    searchClear.classList.add('visible');
+    
+  } else {
+    
+    // hide clear button
+    searchClear.classList.remove('visible');
+    
+  }
+  
+})
+
+// clear search input when clicked on button
+searchClear.addEventListener('click', () => {
+  
+  // clear search input
+  searchInput.innerText = '';
+  
+  // show all files
+  let files = fileWrapper.querySelectorAll('.item[style="display: none;"]');
+  files.forEach(file => { file.style.display = '' });
+  
+  // hide clear button
+  searchClear.classList.remove('visible');
+  
+  // focus search input
+  searchInput.focus();
+  
+})
