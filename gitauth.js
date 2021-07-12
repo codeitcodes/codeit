3558f18c846e31a577318d4c8c73a8ba1142e7e3
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-07-12 11:51:16 +0300

    Update and rename codegit.js to gitauth.js

---
+++ gitauth.js
@@ -0,0 +1,76 @@
+/*
+  github
+*/
+
+// Github login
+const clientId = '7ede3eed3185e59c042d';
+
+let githubToken, treeLoc;
+
+window.onload = () => {
+  
+  githubToken = getStorage('token');
+  treeLoc = getStorage('tree') ? getStorage('tree').split(',') : ['', '', ''];
+
+  loginButton.addEventListener('click', () => {
+  
+    window.open('https://github.com/login/oauth/authorize?client_id='+ clientId +'&scope=repo', 'Login with Github', 'height=575,width=575');
+  
+  })
+  
+  // if not logged into Github
+  if (githubToken == null) {
+
+    // show intro screen
+    sidebar.classList.add('intro');
+
+  } else {
+
+    // render files
+    renderFiles();
+
+  }
+  
+  // if redirected from Github auth
+  window.addEventListener('message', (event) => {
+    
+    // hide intro screen
+    sidebar.classList.remove('intro');
+    
+    var githubCode = event.data;
+    
+    // get Github token
+    getGithubToken(githubCode);
+  
+  })
+  
+  loadCodeFromStorage();
+  
+}
+
+async function getGithubToken(githubCode) {
+  
+  // CORS post to Github with clientId, clientSecret and code
+  var resp = await axios.post('https://sceptercors.herokuapp.com/' +
+                              'https://github.com/login/oauth/access_token?' +
+                              'client_id=' + clientId +
+                              '&client_secret=c1934d5aab1c957800ea8e84ce6a24dda6d68f45' +
+                              '&code=' + githubCode);
+  
+  // save token to localStorage
+  githubToken = resp.access_token;
+  setStorage('token', githubToken);
+  
+  /*
+  // get username
+  var user = await axios.get('https://api.github.com/user', githubToken);
+  
+  // save location in filetree
+  treeLoc = [user.login, '', ''];
+  setStorage('tree', treeLoc.join());
+  */
+  
+  // render files
+  renderFiles();
+  
+}
