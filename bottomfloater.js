39ae0a86b72bca5f410f7023b32306fdf4874ef7
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-07-24 20:44:25 +0300

    Update bottomfloater.js

--- bottomfloater.js
+++ bottomfloater.js
@@ -12,8 +12,13 @@ function updateFloat() {
   // show selected file name
   floatLogo.innerText = selectedFile.name;
   
-  // focus codeit
-  cd.textarea.focus();
+  // when sidebar animation finished
+  window.setTimeout(() => {
+    
+    // focus codeit
+    cd.textarea.focus();
+    
+  }, 400);
   
 }
 
@@ -24,3 +29,18 @@ sidebarOpen.addEventListener('click', () => {
   body.classList.add('expanded');
   
 })
+
+
+// show bottom float when scrolled up
+let lastScrollTop = 0;
+
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+cd.addEventListener('scroll', function() {
+   var st = window.pageYOffset || document.documentElement.scrollTop;
+   if (st > lastScrollTop){
+      // downscroll code
+   } else {
+      // upscroll code
+   }
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
