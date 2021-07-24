06cbfb14fd78c6da24913e090ba597151baba810
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-07-24 15:35:41 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -70,6 +70,10 @@ function load() {
 
 
 
+// is mobile
+const isMobile = window.matchMedia('@media only screen and (max-width: 760px), (min-device-width: 768px) and (max-device-width: 1024px)').matches;
+
+
 // localStorage
 
 let getStorage = (item) => {
