e0226483420beda5b12997913c0d694d548b3725
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-07-24 22:23:17 +0300

    Create codelearn.js

---
+++ codelearn.js
@@ -0,0 +1,18 @@
+
+learnFork.addEventListener('click', () => {
+  
+  window.location.href = 'https://github.com/barhatsor/codeit-app';
+  
+})
+
+learnTutorial.addEventListener('click', () => {
+  
+  window.location.href = 'https://github.com/barhatsor/codeit-app';
+  
+})
+
+learnClose.addEventListener('click', () => {
+  
+  sidebar.classList.remove('learn');
+  
+})
