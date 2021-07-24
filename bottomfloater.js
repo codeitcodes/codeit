d9deb62362cdd5fa2adf9b9e11a573ac89acfa8b
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-07-24 15:53:32 +0300

    Create bottomfloater.js

---
+++ bottomfloater.js
@@ -0,0 +1,23 @@
+
+// function runs on updateFile() in gitsidebar.js
+// if isMobile
+function updateFloat() {
+  
+  // close sidebar
+  body.classList.remove('expanded');
+  
+  // show bottom floater
+  bottomFloat.classList.remove('hidden');
+  
+  // show selected file name
+  floatLogo.innerText = selectedFile.name;
+  
+}
+
+
+// open sidebar when clicked on button
+sidebarOpen.addEventListener('click', () => {
+  
+  body.classList.add('expanded');
+  
+})
