a1e84c3e517c8bc43f7aa45ac3fdc6f7f7ff8dbb
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-08-02 11:19:09 +0300

    Update codeit.prism.js

--- src/codeit.prism.js
+++ src/codeit.prism.js
@@ -23,101 +23,58 @@ class CodeitElement extends HTMLElement {
     let cd = this;
     
     // add codeit CSS to head
-    const css = `cd-el{display:inline-flex;position:relative;overflow:auto;font-size:14px;line-height:1.5;font-family:monospace;text-rendering:optimizeLegibility;font-feature-settings:"kern";background:#f1f3f4;color:#333;border-radius:5px;padding:5px;cursor:text}cd-el textarea{background:0 0;color:transparent;position:absolute;border:0;resize:none;font:inherit;letter-spacing:inherit;line-height:inherit;outline:0;caret-color:#000;white-space:pre;display:inline-table;padding:0;min-width:1px;box-sizing:border-box;z-index:1}cd-el pre{margin:0;font:inherit;height:max-content;width:max-content}cd-el code{font:inherit}code[class*=language-],pre[class*=language-]{color:#fff;background:0 0;text-shadow:0 -.1em .2em #000;font-family:Consolas,Monaco,'Andale Mono','Ubuntu Mono',monospace;font-size:1em;text-align:left;white-space:pre;word-spacing:normal;word-break:normal;word-wrap:normal;line-height:1.5;-moz-tab-size:4;-o-tab-size:4;tab-size:4;-webkit-hyphens:none;-moz-hyphens:none;-ms-hyphens:none;hyphens:none}@media print{code[class*=language-],pre[class*=language-]{text-shadow:none}}:not(pre)>code[class*=language-],pre[class*=language-]{background:#4c3f33}pre[class*=language-]{padding:1em;margin:.5em 0;overflow:auto;border:.3em solid #7a6651;border-radius:.5em;box-shadow:1px 1px .5em #000 inset}:not(pre)>code[class*=language-]{padding:.15em .2em .05em;border-radius:.3em;border:.13em solid #7a6651;box-shadow:1px 1px .3em -.1em #000 inset;white-space:normal}.token.cdata,.token.comment,.token.doctype,.token.prolog{color:#997f66}.token.punctuation{opacity:.7}.token.namespace{opacity:.7}.token.boolean,.token.constant,.token.number,.token.property,.token.symbol,.token.tag{color:#d1939e}.token.attr-name,.token.builtin,.token.char,.token.inserted,.token.selector,.token.string{color:#bce051}.language-css .token.string,.style .token.string,.token.entity,.token.operator,.token.url,.token.variable{color:#f4b73d}.token.atrule,.token.attr-value,.token.keyword{color:#d1939e}.token.important,.token.regex{color:#e90}.token.bold,.token.important{font-weight:700}.token.italic{font-style:italic}.token.entity{cursor:help}.token.deleted{color:red}`,
+    const css = `cd-el{outline:0;user-select:text;-webkit-user-select:text;overflow-wrap:break-word;white-space:pre-wrap;overflow:auto;font-size:14px;line-height:1.5;font-family:monospace;text-rendering:optimizeLegibility;font-feature-settings:"kern";display:block;background:#f1f3f4;color:#333;border-radius:10px;padding:10px;cursor:text;tab-size:2}code[class*=language-],pre[class*=language-]{color:#fff;background:0 0;text-shadow:0 -.1em .2em #000;font-family:Consolas,Monaco,'Andale Mono','Ubuntu Mono',monospace;font-size:1em;text-align:left;white-space:pre;word-spacing:normal;word-break:normal;word-wrap:normal;line-height:1.5;-moz-tab-size:4;-o-tab-size:4;tab-size:4;-webkit-hyphens:none;-moz-hyphens:none;-ms-hyphens:none;hyphens:none}@media print{code[class*=language-],pre[class*=language-]{text-shadow:none}}:not(pre)>code[class*=language-],pre[class*=language-]{background:#4c3f33}pre[class*=language-]{padding:1em;margin:.5em 0;overflow:auto;border:.3em solid #7a6651;border-radius:.5em;box-shadow:1px 1px .5em #000 inset}:not(pre)>code[class*=language-]{padding:.15em .2em .05em;border-radius:.3em;border:.13em solid #7a6651;box-shadow:1px 1px .3em -.1em #000 inset;white-space:normal}.token.cdata,.token.comment,.token.doctype,.token.prolog{color:#997f66}.token.punctuation{opacity:.7}.token.namespace{opacity:.7}.token.boolean,.token.constant,.token.number,.token.property,.token.symbol,.token.tag{color:#d1939e}.token.attr-name,.token.builtin,.token.char,.token.inserted,.token.selector,.token.string{color:#bce051}.language-css .token.string,.style .token.string,.token.entity,.token.operator,.token.url,.token.variable{color:#f4b73d}.token.atrule,.token.attr-value,.token.keyword{color:#d1939e}.token.important,.token.regex{color:#e90}.token.bold,.token.important{font-weight:700}.token.italic{font-style:italic}.token.entity{cursor:help}.token.deleted{color:red}`,
           head = document.head,
           style = document.createElement('style');
     head.appendChild(style);
     style.appendChild(document.createTextNode(css));
     
-    // create codeit elements
-    cd.textarea = document.createElement('textarea');
-
-    cd.pre = document.createElement('pre');
-    cd.code = document.createElement('code');
+    // if editable property is true
+    cd.editable = (cd.getAttribute('editable') == 'false') ? false : true;
     
-    // style codeit textarea
-    cd.textarea.setAttribute('spellcheck', 'false');
-    cd.textarea.setAttribute('autocorrect', 'off');
-    cd.textarea.setAttribute('autocomplete', 'off');
-    cd.textarea.setAttribute('aria-autocomplete', 'list');
-    cd.textarea.setAttribute('autocapitalize', 'off');
-    
-    cd.textarea.setAttribute('rows', 1);
+    if (cd.editable) {
+      
+      // make codeit editable
+      cd.setAttribute('contenteditable', 'plaintext-only');
+      cd.setAttribute('spellcheck', 'false');
+      cd.setAttribute('autocorrect', 'off');
+      cd.setAttribute('autocomplete', 'off');
+      cd.setAttribute('aria-autocomplete', 'list');
+      cd.setAttribute('autocapitalize', 'off');
+      cd.setAttribute('data-gramm', 'false');
+      
+    }
 
     // highlight with specified lang
-    cd.code.classList = (cd.getAttribute('lang') != undefined) ? cd.getAttribute('lang') : 'js';
+    cd.lang = (cd.getAttribute('lang') != undefined) ? cd.getAttribute('lang') : 'js';
 
     // parse code
-    cd.parsedCode = cd.innerText.replace(/^\n|\n$/g, '');
+    cd.textContent = cd.textContent.replace(/^\n|\n$/g, '');
 
-    // clear codeit
-    cd.innerHTML = '';
+    // highlight codeit
+    cd.highlight = () => {
 
-    // append codeit elements to DOM
-    cd.appendChild(cd.textarea);
-    cd.appendChild(cd.pre);
-    cd.pre.appendChild(cd.code);
-
-    // set codeit textarea value to code
-    cd.textarea.value = cd.parsedCode;
-
-    // init codeit behavior
-    new Behave({
-      textarea: cd.textarea,
-      replaceTab: true,
-      softTabs: true,
-      tabSize: 2,
-      autoOpen: true,
-      overwrite: true,
-      autoStrip: true,
-      autoIndent: true
-    });
-
-    // update codeit
-    
-    cd.setValue = (code) => {
-
-      cd.textarea.value = code;
-      cd.update();
-
-    }
-    
-    cd.update = () => {
-      
-      //cd.code.innerHTML = cd.textarea.value;
-      
-      // resize textarea
-      cd.textarea.style.width = cd.pre.clientWidth + 'px';
-      cd.textarea.style.height = cd.pre.clientHeight + 'px';
-      
-      window.setTimeout(() => {
+      cd.debounce(() => {
         
         // Returns a highlighted HTML string
-        const html = Prism.highlight(cd.textarea.value, Prism.languages.javascript, 'javascript');
+        const html = Prism.highlight(cd.textContent, Prism.languages.javascript, 'javascript');
 
-        cd.code.innerHTML = html;
+        cd.innerHTML = html;
         
       }, 30);
-
+    
     }
     
-    const escapeHTML = (unsafe) => {
-      return unsafe
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#039;");
+    cd.debounce = (func, time) => {
+      
+      window.setTimeout(func, time);
+      
     }
-
-    cd.textarea.addEventListener('input', cd.update);
-    cd.textarea.addEventListener('keydown', cd.update);
     
-    BehaveHooks.add('openChar:after', cd.update);
-    
+    /*
     BehaveHooks.add('enter:after', (data) => {
       
-      setTimeout(() => {
+      cd.debounce(() => {
         
         // if pressed enter on one of last three lines, scroll down
         const firstLine = (data.lines.current == data.lines.total),
@@ -130,20 +87,286 @@ class CodeitElement extends HTMLElement {
         
       }, 0);
       
-    });
+    });*/
     
-    // focus codeit when clicked
-    if (cd.getAttribute('editable') != 'false') {
+    const escapeHTML = (unsafe) => {
+      return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+    }
+    
+  // create a new instance of 'MutationObserver' named 'observer',
+  // passing it a callback function
+
+  let updatingHighlight = false;
+
+  let textContentObserver = new MutationObserver(function(mutationsList, observer) {
+
+    // run on all mutations
+    mutationsList.forEach(mutation => {
+
+      /*console.log(mutation.type);
+      // if mutation was a textNode mutation
+      if (mutation.type == 'characterData') {
+        // if the text didn't change, stop
+        if (mutation.oldValue == cd.innerText) {
+          textChanged = false;
+          console.log('Text: ' + mutation.oldValue, cd.innerText);
+        }
+      } else if (mutation.type == 'childList') { // if mutation was a innerHTML mutation
+        // if mutation was from highlighting, stop
+        if (updatingHighlight == true) {
+          updatingHighlight = false;
+          textChanged = false;
+        } else {
+          updatingHighlight = true;
+          textChanged = true;
+        }
+      }*/
+
+    });
+
+    if (updatingHighlight == false) {
+
+      //console.log('Updating Codeit from textContent', mutationsList);
+      updatingHighlight = true;
+      cd.update();
+
+    } else {
+
+      //console.log('Ignoring update from highlight');
+      updatingHighlight = false;
+
+    }
+
+    //console.log('textContent', mutationsList);
+
+  });
+
+
+  let innerHTMLObserver = new MutationObserver(function(mutationsList, observer) {
+
+    // run on all mutations
+    mutationsList.forEach(mutation => {
+
+      /*console.log(mutation.type);
+      // if mutation was a textNode mutation
+      if (mutation.type == 'characterData') {
+        // if the text didn't change, stop
+        if (mutation.oldValue == cd.innerText) {
+          textChanged = false;
+          console.log('Text: ' + mutation.oldValue, cd.innerText);
+        }
+      } else if (mutation.type == 'childList') { // if mutation was a innerHTML mutation
+        // if mutation was from highlighting, stop
+        if (updatingHighlight == true) {
+          updatingHighlight = false;
+          textChanged = false;
+        } else {
+          updatingHighlight = true;
+          textChanged = true;
+        }
+      }*/
+
+    });
+
+    //console.log('Updating Codeit from innerHTML', mutationsList);
+    updatingHighlight = true;
+    cd.update();
+    //console.log('innerHTML', mutationsList);
+
+  });
+
+
+
+  // call 'observe' on that MutationObserver instance,
+  // passing it the element to observe, and the options object
+
+  var textContentConfig = { characterData: false, attributes: false, childList: true, subtree: false };
+  textContentObserver.observe(cd, textContentConfig);
+
+  var innerHTMLConfig = { characterData: true, attributes: false, childList: false, subtree: true };
+  innerHTMLObserver.observe(cd, innerHTMLConfig);
+  
+  
+  cd.addEventListener('keydown', (event) => {
+
+    handleNewline(event);
+
+  })
+  
+  function handleNewline(event) {
+    
+    // trap the return key being pressed
+    if (event.key === 'Enter') {
+
+      // prevent the default behaviour of return key pressed
+      preventDefault(event);
+      event.stopPropagation();
       
-      cd.addEventListener('click', () => {
-
-        cd.textarea.focus();
-
-      });
+      // add newline
+      cd.insert('\n');
       
     }
     
-    cd.update();
+  }
+
+
+  var nodePath = [];
+
+  function getCaretPosInInnerText(el) {
+
+    var sel = window.getSelection();
+
+    var targetNode = sel.anchorNode; // node of caret
+    var caretOffset = sel.anchorOffset; // offset in node
+
+    var overallLength = 0;
+
+    //console.log(el, targetNode, caretOffset);
+
+    function getTextNodes(node) {
+
+      //If reached the target node:
+      if (node != targetNode) {
+
+        //If node type is text:
+        if (node.nodeType == 3) {
+          //if(node.nodeValue === ' ') overallLength += 1;
+
+          nodePath.push(node);
+
+          overallLength += node.nodeValue.length;
+          //console.log(node.nodeValue)
+          //console.log('node length',node.nodeValue.length)
+
+          //if (node.nodeValue === ' ') {
+        } else { //if it's an empty node, this means more nodes underneath:
+          //Go over his brother leaves:
+          for (var i = 0, len = node.childNodes.length; i < len; ++i)
+          {
+            // Call recursive call on node's children:
+            getTextNodes(node.childNodes[i]);
+
+          }
+        }
+      }else{
+        console.log('found node', node);
+
+        if (node.nodeType != 3) {
+          console.log('bad node');
+          overallLength = cd.lastChild.value.length;
+          caretOffset = 0;
+        }
+      }
+    }
+
+    getTextNodes(el);
+
+    return overallLength + caretOffset;
+
+  }
+
+
+  function setCaret(el, pos) {
+    var range = document.createRange();
+    var sel = window.getSelection();
+    range.setStart(el,pos);
+    range.collapse(true);
+    sel.removeAllRanges();
+    sel.addRange(range);
+  }
+
+
+  //Go over all nodes till reach the number of characters:
+  function getTextNodesIn(el, includeWhitespaceNodes) {
+
+    var overallLength = 0, lastNode;
+
+    function getTextNodes(node) {
+
+      //If reached the target text:
+      if (overallLength <= caretPosInText) {
+
+        lastNode = node;
+
+        //If node type is text:
+        if (node.nodeType == 3)
+        {
+
+          overallLength += node.nodeValue.length;
+
+          //if (node.nodeValue === ' ') {
+        } else { //if it's an empty node, this means more nodes underneath:
+
+          //Go over his brother leaves:
+          for (var i = 0, len = node.childNodes.length; i < len; ++i)
+          {
+            // Call recursive call on node's children:
+            getTextNodes(node.childNodes[i]);
+
+          }
+        }
+      }else{
+        //console.log('overall length:', overallLength);
+      }
+    }
+
+    getTextNodes(el);
+
+    if (lastNode) {
+
+      var lastNodeLength = lastNode.nodeValue.length;
+
+      //end of line problem
+      //sel.anchorOffset
+      //return [lastNode, (lastNodeLength - (overallLength - caretPosInText))];
+      return [lastNode, (lastNodeLength - (overallLength - caretPosInText))];
+
+    } else {
+
+      return [cd, 0];
+
+    }
+  }
+
+
+  let caretPosInText = 0;
+  let prev;
+
+  cd.update = () => {
+    
+    if (cd.textContent !== prev) {
+      
+      caretPosInText = getCaretPosInInnerText(cd);
+
+      if (caretPosInText) {
+
+        cd.highlight();
+
+
+        let caretData = getTextNodesIn(cd);
+
+        //console.log('caretData:',caretData[1], 'node:',cd.innerHTML.length);
+
+        setCaret(caretData[0], caretData[1]);
+
+      } else {
+
+        cd.highlight();
+
+      }
+      
+    }
+    
+    prev = cd.textContent;
+
+  }
+    
+    cd.highlight();
     
   }
   
