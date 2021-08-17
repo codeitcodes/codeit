52e9e57c66b5f165d7b125dd13fa0de89bf42b97
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-08-17 18:33:49 +0300

    Update and rename dynamicmanifest.js to manifest.js

--- manifest.js
+++ manifest.js
@@ -4,6 +4,7 @@
 let manifest = {
   "name": "Codeit",
   "short_name": "Codeit",
+  "description": "Mobile code editor connected to Github.",
   "background_color": "#313744",
   "theme_color": "#313744",
   "display": "fullscreen",
