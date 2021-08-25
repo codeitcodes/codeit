0fd3846fd35ae8528e1454c185c64aa0469b307c
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-08-25 19:35:32 +0300

    Update gitapi.js

--- git/gitapi.js
+++ git/gitapi.js
@@ -1,70 +1,71 @@
 
-// push file
-// function pushes file to git
+let git = {
 
-async function pushFile(file, commit) {
-  
-  let content;
-  
-  // if updating an existing file
-  if (file.sha) {
+  // push file
+  // function pushes file to git
+  'push': async (file, commit) => {
 
-    // if currently editing file
-    if (file.selected) {
+    let content;
 
-      // get current value of file
-      content = btoa(cd.textContent);
+    // if updating an existing file
+    if (file.sha) {
 
-    } else { // else, load from storage
+      // if currently editing file
+      if (file.selected) {
 
-      content = modifiedFiles[file.sha][0];
+        // get current value of file
+        content = btoa(cd.textContent);
+
+      } else { // else, load from storage
+
+        content = modifiedFiles[file.sha][0];
+
+      }
+
+    } else { // if creating a new file
+
+      content = file.content;
+      file.sha = '';
 
     }
-    
-  } else { // if creating a new file
-    
-    content = file.content;
-    file.sha = '';
-    
+
+
+    let query = 'https://api.github.com/repos/' +
+                treeLoc[0] +
+                '/' + treeLoc[1] +
+                '/contents' + treeLoc[2] + '/' + file.name;
+
+    let commitData = {
+      message: commit.message,
+      content: content,
+      sha: file.sha
+    };
+
+    // commit file
+    var resp = await axios.put(query, githubToken, commitData);
+
+
+    // if updating an existing file
+    if (file.sha) {
+
+      // force-update cache
+      var newFile = await axios.get(query, githubToken, file.sha);
+
+      // delete file from modified files
+      deleteModifiedFile(file.sha);
+
+      file.element.classList.remove('modified');
+
+    }
+
+    return resp.content.sha;
+
+  },
+
+  // create repo (indev)
+  // function creates a new repository
+  createRepo: async (repo) => {
+
   }
-  
-  
-  let query = 'https://api.github.com/repos/' +
-              treeLoc[0] +
-              '/' + treeLoc[1] +
-              '/contents' + treeLoc[2] + '/' + file.name;
-  
-  let commitData = {
-    message: commit.message,
-    content: content,
-    sha: file.sha
-  };
 
-  // commit file
-  var resp = await axios.put(query, githubToken, commitData);
-  
-  
-  // if updating an existing file
-  if (file.sha) {
-  
-    // force-update cache
-    var newFile = await axios.get(query, githubToken, file.sha);
-
-    // delete file from modified files
-    deleteModifiedFile(file.sha);
-
-    file.element.classList.remove('modified');
-
-  }
-  
-  return resp.content.sha;
-  
-}
-
-
-// create repo
-// function creates a new repository
-// (indev)
-function createRepo(repo) {
-  
-}
+};
