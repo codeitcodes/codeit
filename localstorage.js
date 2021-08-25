9cf552e34b244cacd9327788d19deaf813ab85b1
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-08-25 19:17:08 +0300

    Update and rename codestorage.js to localstorage.js

---
+++ localstorage.js
@@ -0,0 +1,76 @@
+
+let modifiedFiles, selectedFile;
+
+
+function changeSelectedFileLS(newSelectedFile) {
+  
+  setStorage('selectedFile', JSON.stringify(newSelectedFile));
+  
+}
+
+
+function saveModifiedFileLS(modifiedFile) {
+  
+  modifiedFiles[modifiedFile.sha] = modifiedFile;
+  
+  setStorage('modifiedFiles', JSON.stringify(modifiedFiles));
+  
+}
+
+
+function deleteModifiedFileLS(fileSha) {
+    
+  delete modifiedFiles[fileSha];
+  
+  setStorage('modifiedFiles', JSON.stringify(modifiedFiles));
+  
+}
+
+
+function loadLS() {
+  
+  // load modified files from storage
+  modifiedFiles = getStorage('modifiedFiles') ? JSON.parse(getStorage('modifiedFiles')) : {};
+  
+  // load selected file from storage
+  selectedFile = getStorage('selectedFile') ? JSON.parse(getStorage('selectedFile')) : {dir: '', sha: ''};
+    
+  setupEditor();
+  
+  setupSidebar();
+  
+}
+
+function saveBeforeUnloadLS() {
+  
+  // set localStorage values
+  saveCodeLS();
+  saveCodePosLS();
+  
+}
+
+window.onbeforeunload = saveBeforeUnloadLS;
+
+function saveCodeLS() {
+  
+  setStorage('code', btoa(cd.textContent));
+  
+}
+
+function saveCodePosLS() {
+  
+  setStorage('caret', cd.getSelection().start);
+  setStorage('scrollPos', (cd.scrollLeft + ',' + cd.scrollTop));
+  
+}
+
+function saveCodeLangLS() {
+  
+  setStorage('lang', cd.lang);
+  
+}
+
+function saveSidebarStateLS() {
+  
+  setStorage('sidebar', body.classList.contains('expanded'));
+}
