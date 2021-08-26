0ad4b7c999c751b9ba1a64b4512f914e01ab092e
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-08-26 21:18:05 +0300

    Update prism-line-numbers.js

--- src/prism-line-numbers.js
+++ src/prism-line-numbers.js
@@ -63,9 +63,7 @@
 		 * @returns {void}
 		 */
 		resize: function (element) {
-			window.setTimeout(() => {
-				addLineRows(element);
-			}, 20);
+			addLineRows(element);
 		},
 
 		/**
@@ -87,96 +85,99 @@
 	 * @param {HTMLElement[]} elements
 	 */
 	function resizeElements(elements) {
-		elements = elements.filter(function (e) {
-			var codeStyles = getStyles(e);
-			var whiteSpace = codeStyles['white-space'];
-			return whiteSpace === 'pre-wrap' || whiteSpace === 'pre-line';
-		});
+		window.setTimeout(() => {
+			console.log('here');
+			elements = elements.filter(function (e) {
+				var codeStyles = getStyles(e);
+				var whiteSpace = codeStyles['white-space'];
+				return whiteSpace === 'pre-wrap' || whiteSpace === 'pre-line';
+			});
 
-		if (elements.length == 0) {
-			return;
-		}
-
-		var infos = elements.map(function (element) {
-			var codeElement = element;
-			var lineNumbersWrapper = element.querySelector('.line-numbers-rows');
-			if (!codeElement || !lineNumbersWrapper) {
-				return undefined;
+			if (elements.length == 0) {
+				return;
 			}
 
-			/** @type {HTMLElement} */
-			var lineNumberSizer = element.querySelector('.line-numbers-sizer');
-			var codeLines = codeElement.textContent.split(NEW_LINE_EXP);
+			var infos = elements.map(function (element) {
+				var codeElement = element;
+				var lineNumbersWrapper = element.querySelector('.line-numbers-rows');
+				if (!codeElement || !lineNumbersWrapper) {
+					return undefined;
+				}
 
-			if (!lineNumberSizer) {
-				lineNumberSizer = document.createElement('span');
-				lineNumberSizer.className = 'line-numbers-sizer';
+				/** @type {HTMLElement} */
+				var lineNumberSizer = element.querySelector('.line-numbers-sizer');
+				var codeLines = codeElement.textContent.split(NEW_LINE_EXP);
 
-				codeElement.appendChild(lineNumberSizer);
-			}
+				if (!lineNumberSizer) {
+					lineNumberSizer = document.createElement('span');
+					lineNumberSizer.className = 'line-numbers-sizer';
 
-			lineNumberSizer.innerHTML = '0';
-			lineNumberSizer.style.display = 'block';
+					codeElement.appendChild(lineNumberSizer);
+				}
 
-			var oneLinerHeight = lineNumberSizer.getBoundingClientRect().height;
-			lineNumberSizer.innerHTML = '';
+				lineNumberSizer.innerHTML = '0';
+				lineNumberSizer.style.display = 'block';
 
-			return {
-				element: element,
-				lines: codeLines,
-				lineHeights: [],
-				oneLinerHeight: oneLinerHeight,
-				sizer: lineNumberSizer,
-			};
-		}).filter(Boolean);
+				var oneLinerHeight = lineNumberSizer.getBoundingClientRect().height;
+				lineNumberSizer.innerHTML = '';
 
-		infos.forEach(function (info) {
-			var lineNumberSizer = info.sizer;
-			var lines = info.lines;
-			var lineHeights = info.lineHeights;
-			var oneLinerHeight = info.oneLinerHeight;
+				return {
+					element: element,
+					lines: codeLines,
+					lineHeights: [],
+					oneLinerHeight: oneLinerHeight,
+					sizer: lineNumberSizer,
+				};
+			}).filter(Boolean);
 
-			lineHeights[lines.length - 1] = undefined;
-			lines.forEach(function (line, index) {
-				if (line && line.length > 1) {
-					var e = lineNumberSizer.appendChild(document.createElement('span'));
-					e.style.display = 'block';
-					e.textContent = line;
-				} else {
-					lineHeights[index] = oneLinerHeight;
+			infos.forEach(function (info) {
+				var lineNumberSizer = info.sizer;
+				var lines = info.lines;
+				var lineHeights = info.lineHeights;
+				var oneLinerHeight = info.oneLinerHeight;
+
+				lineHeights[lines.length - 1] = undefined;
+				lines.forEach(function (line, index) {
+					if (line && line.length > 1) {
+						var e = lineNumberSizer.appendChild(document.createElement('span'));
+						e.style.display = 'block';
+						e.textContent = line;
+					} else {
+						lineHeights[index] = oneLinerHeight;
+					}
+				});
+			});
+
+			infos.forEach(function (info) {
+				var lineNumberSizer = info.sizer;
+				var lineHeights = info.lineHeights;
+
+				var childIndex = 0;
+				for (var i = 0; i < lineHeights.length; i++) {
+					if (lineHeights[i] === undefined) {
+						lineHeights[i] = lineNumberSizer.children[childIndex++].getBoundingClientRect().height;
+					}
 				}
 			});
-		});
 
-		infos.forEach(function (info) {
-			var lineNumberSizer = info.sizer;
-			var lineHeights = info.lineHeights;
+			infos.forEach(function (info) {
+				var lineNumberSizer = info.sizer;
+				var wrapper = info.element.querySelector('.line-numbers-rows');
 
-			var childIndex = 0;
-			for (var i = 0; i < lineHeights.length; i++) {
-				if (lineHeights[i] === undefined) {
-					lineHeights[i] = lineNumberSizer.children[childIndex++].getBoundingClientRect().height;
-				}
-			}
-		});
+				lineNumberSizer.style.display = 'none';
+				lineNumberSizer.innerHTML = '';
 
-		infos.forEach(function (info) {
-			var lineNumberSizer = info.sizer;
-			var wrapper = info.element.querySelector('.line-numbers-rows');
-
-			lineNumberSizer.style.display = 'none';
-			lineNumberSizer.innerHTML = '';
-
-			info.lineHeights.forEach(function (height, lineNumber) {
-				wrapper.children[lineNumber].style.height = height + 'px';
+				info.lineHeights.forEach(function (height, lineNumber) {
+					wrapper.children[lineNumber].style.height = height + 'px';
+				});
 			});
-		});
-		
-		elements.map(element => {
-			var lineNumbersWrapper = element.querySelector('.line-numbers-rows');
-			
-			element.style.paddingLeft = lineNumbersWrapper.clientWidth + 10 + 'px';
-		});
+
+			elements.map(element => {
+				var lineNumbersWrapper = element.querySelector('.line-numbers-rows');
+
+				element.style.paddingLeft = lineNumbersWrapper.clientWidth + 10 + 'px';
+			});
+		}, 20);
 	}
 
 	/**
@@ -203,8 +204,6 @@
 	});
 
 	function addLineRows(element) {
-		console.log('here');
-		
 		// Abort if line numbers already exists
 		if (element.querySelector('.line-numbers-rows')) {
 			return;
@@ -234,14 +233,7 @@
 		
 		var code = /** @type {Element} */ (env.element);
 		
-		// only add line numbers if <code> or one of its ancestors has the `line-numbers` class
-		if (!Prism.util.isActive(code, PLUGIN_NAME)) {
-			return;
-		}
-		
-		window.setTimeout(() => {
-			addLineRows(code);
-		}, 20);
+		addLineRows(code);
 	});
 
 	Prism.hooks.add('line-numbers', function (env) {
