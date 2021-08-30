f6c550537c7e8d9e9444bb47e4af20cacb9f470f
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-08-30 09:20:55 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -89,12 +89,14 @@ function load() {
 
 
 // is mobile
-//const isMobile = window.matchMedia('only screen and (hover: none) and (pointer: coarse)').matches;
-const isMobile = window.matchMedia('only screen and (max-width: 760px), (min-device-width: 768px) and (max-device-width: 1024px)').matches;
+let isMobile = window.matchMedia('only screen and (max-width: 760px), (min-device-width: 768px) and (max-device-width: 1024px)').matches;
 
-if (isMobile) {
-  body.classList.add('mobile');
-}
+// for debugging
+window.addEventListener('resize', () => {
+  
+  isMobile = window.matchMedia('only screen and (max-width: 760px), (min-device-width: 768px) and (max-device-width: 1024px)').matches;
+  
+});
 
 
 // localStorage
