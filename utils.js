3536fba6d960aa096584d7864103dbeb186262cb
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-08-30 11:08:19 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -102,6 +102,26 @@ window.addEventListener('resize', () => {
 });
 
 
+// base64 encode/decode
+
+let encodeUnicode = (str) => {
+  // first we use encodeURIComponent to get percent-encoded UTF-8,
+  // then we convert the percent encodings into raw bytes which
+  // can be fed into btoa.
+  return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+      function toSolidBytes(match, p1) {
+          return String.fromCharCode('0x' + p1);
+  }));
+}
+
+let decodeUnicode = (str) => {
+  // going backwards: from bytestream, to percent-encoding, to original string.
+  return decodeURIComponent(atob(str).split('').map(function (c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+}
+
+
 // localStorage
 
 let getStorage = (item) => {
