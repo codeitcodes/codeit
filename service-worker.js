c0fb651afd0f9ed36bdcd64323989381bde2e51b
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2021-10-01 19:53:56 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // update cache names any time any of the cached files change
-const CACHE_NAME = 'static-cache-v114';
+const CACHE_NAME = 'static-cache-v115';
 
 // list of files to cache
 const FILES_TO_CACHE = [
