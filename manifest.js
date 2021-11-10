3f0f7b95174d07620e158fea034b20eb165d8f30
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-11-10 14:09:32 +0200

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -65,7 +65,7 @@ if (!isMobile) {
   }
   
   // change page favicon
-  var link = document.querySelector('link[rel*="icon"]');
+  var link = document.querySelector("link[rel*='icon']");
   link.href = '/icons/app-favicon.png';
   
 } else if (!isSafari) {
@@ -75,22 +75,11 @@ if (!isMobile) {
 }
 
 
-function updateManifest() {
-  
-  let linkElem = document.querySelector('link[rel="manifest"]')
-                 || document.createElement('link');
-                
-  linkElem.setAttribute('rel', 'manifest');
-  linkElem.setAttribute('href', 'data:application/json,' + encodeURIComponent(JSON.stringify(manifest)));
-  
-  if (!linkElem.parentElement) {
-    
-    document.head.appendChild(linkElem);
-    
-  }
-  
-}
-
 
 // apply dynamic manifest
-updateManifest();
+
+let linkElem = document.createElement('link');
+linkElem.setAttribute('rel', 'manifest');
+linkElem.setAttribute('href', 'data:application/json,' + encodeURIComponent(JSON.stringify(manifest)));
+
+document.head.appendChild(linkElem);
