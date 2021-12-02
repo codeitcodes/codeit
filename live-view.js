b534e12328173baa7bc32030849e3f861a75c0aa
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-12-02 23:12:02 +0200

    Update live-view.js

--- live-view.js
+++ live-view.js
@@ -105,7 +105,7 @@ function addBottomSwipeListener() {
           
           toggleLiveView(selectedFile);
           
-        } else if (swiped && liveWrapper.innerHTML == '') {
+        } else if (swiped && liveView.innerHTML == '') {
           
           // if swiped down and bottom float isn't expanded
           // hide bottom float
