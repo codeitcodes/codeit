6d1845cf259916d3715dcee340d14430022972c6
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-12-29 14:47:11 +0200

    Update live-view.js

--- live-view.js
+++ live-view.js
@@ -212,7 +212,7 @@ function addBottomSwipeListener() {
         
         const shareData = {
           title: 'Share live view',
-          text: 'Open ' + treeLoc[0] + '/' + treeLoc[1] + ' with Codeit: ' +
+          text: 'Run ' + treeLoc[0] + '/' + treeLoc[1] + ' with Codeit: ' +
                 window.location.origin + '/full?q=' + encodeURIComponent(treeLoc.join(',') + '+' + selectedFile.name + ',' + selectedFile.sha) + '&l=true'
         }
         
