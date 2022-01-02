696d656c3f6454d0fbfcf8b6562b21e76908a589
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-01-02 14:50:59 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // update cache names any time any of the cached files change
-const CACHE_NAME = 'static-cache-v268';
+const CACHE_NAME = 'static-cache-v269';
 
 // list of files to cache
 const FILES_TO_CACHE = [
