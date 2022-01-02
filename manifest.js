9746f7db9a6a7fa0a8b24a0fe3c483a09398aa57
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-01-02 14:44:16 +0200

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -36,7 +36,7 @@ let manifest = {
     }
   ],
   "share_target": {
-    "action": "/full?file=true",
+    "action": window.location.origin + "/full?file=true",
     "method": "GET",
     "enctype": "multipart/form-data",
     "params": {
