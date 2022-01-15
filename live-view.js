7f3d589ae39136c99167b896a5af628e58015189
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-01-15 19:47:53 +0200

    Update live-view.js

--- live-view.js
+++ live-view.js
@@ -367,11 +367,24 @@ if (isMobile) {
 
 } else {
   
-  liveToggle.addEventListener('click', () => {
+  liveToggle.querySelector('.arrow').addEventListener('click', () => {
     
     // toggle live view
     liveView.classList.toggle('visible');
     toggleLiveView(selectedFile);
+    
+  });
+  
+  liveToggle.querySelector('.share').addEventListener('click', () => {
+    
+    // if clicked on share button,
+    // share live view URL
+
+    const shareData = 'Run ' + treeLoc[0] + '/' + treeLoc[1] + ' with Codeit: ' +
+                      window.location.origin + '/full?q=' + encodeURIComponent(treeLoc.join(',') + '+' + selectedFile.name + ',' + selectedFile.sha) + '&l=true';
+
+    copy(shareData.text);
+    alert('Copied link to clipboard.');
     
   });
   
