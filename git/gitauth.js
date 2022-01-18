b3f13d629975cda6c69e3ff693f132bf3300778f
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-01-18 12:00:44 +0200

    A

--- git/gitauth.js
+++ git/gitauth.js
@@ -10,16 +10,16 @@ let gitToken, treeLoc, authUser;
 window.onload = async () => {
 
   gitToken = getStorage('gitToken') ?? '';
-  
+
   if (gitToken == 'undefined') {
     gitToken = '';
   }
-  
+
   treeLoc = getStorage('tree') ? getStorage('tree').split(',') : ['', '', ''];
-  
-  loggedUser = JSON.parse(getStorage('loggedUser'));
-  
-  
+
+  loggedUser = if (getStorage('loggedUser')) JSON.parse(getStorage('loggedUser')) : false;
+
+
   loginButton.addEventListener('click', () => {
 
     window.open('https://github.com/login/oauth/authorize?client_id='+ clientId +'&scope=repo,user,write:org', 'Login with Github', 'height=575,width=575');
@@ -31,18 +31,18 @@ window.onload = async () => {
 
     // hide intro screen
     sidebar.classList.remove('intro');
-    
+
     // if on safari, refresh header color
     if (isSafari) {
-      
+
       document.querySelector('meta[name="theme-color"]').content = '#313744';
-      
+
       onNextFrame(() => {
-        
+
         document.querySelector('meta[name="theme-color"]').content = '#1a1c24';
-        
+
       });
-      
+
     }
 
     // start loading
@@ -54,8 +54,8 @@ window.onload = async () => {
     getGithubToken(gitCode);
 
   })
-  
-  
+
+
   loadLS();
 
 }
@@ -68,19 +68,19 @@ async function getGithubToken(gitCode) {
                                'client_id=' + clientId +
                                '&client_secret=c1934d5aab1c957800ea8e84ce6a24dda6d68f45' +
                                '&code=' + gitCode);
-  
+
   // save git token to localStorage
   gitToken = resp.access_token;
   saveGitTokenLS(gitToken);
-  
-  
+
+
   // get logged user
   loggedUser = await axios.get('https://api.github.com/user', gitToken);
-  
+
   // save logged user in local storage
   setStorage('loggedUser', JSON.stringify(loggedUser));
-  
-  
+
+
   // render sidebar
   renderSidebarHTML();
 
