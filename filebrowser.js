a63eeaaad2a634b17c11e8815b9ee53effc32bc4
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-02-02 10:10:13 +0200

    Update filebrowser.js

--- filebrowser.js
+++ filebrowser.js
@@ -770,7 +770,7 @@ async function renderBranchMenuHTML(renderAll) {
     
     // clean resp and save only relevant fields
     let cleanedResp = branchResp.map(branch => {
-      return { name: branch.name, commit: { sha: branch.commit.sha }};
+      return { name, commit.sha };
     });
     
     // save resp in HTML
