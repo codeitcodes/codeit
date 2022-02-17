fe322f17634e72d7a6006fda834873f6cda46c02
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-02-17 10:23:34 +0200

    Update filebrowser.js

--- filebrowser.js
+++ filebrowser.js
@@ -1168,15 +1168,19 @@ optionsButton.addEventListener('click', () => {
 // if clicked on branch icon,
 // toggle branch menu
 sidebarBranch.addEventListener('click', () => {
-  
-  // move branch menu to icon
-  moveElToEl(branchMenu, sidebarBranch);
-  
-  branchMenu.classList.add('top-margin');
-  
+    
   branchMenu.classList.toggle('visible');
   sidebarBranch.classList.toggle('active');
   branchButton.classList.toggle('active');
+  
+  if (branchMenu.classList.contains('visible')) {
+    
+    // move branch menu to icon
+    moveElToEl(branchMenu, sidebarBranch, 13);
+    
+    branchMenu.classList.add('top-margin');
+    
+  }
   
 })
 
@@ -1184,14 +1188,18 @@ sidebarBranch.addEventListener('click', () => {
 // toggle branch menu
 branchButton.addEventListener('click', () => {
   
-  // move branch menu to button
-  moveElToEl(branchMenu, branchButton);
-  
-  branchMenu.classList.remove('top-margin');
-  
   branchMenu.classList.toggle('visible');
   sidebarBranch.classList.toggle('active');
   branchButton.classList.toggle('active');
+  
+  if (branchMenu.classList.contains('visible')) {
+    
+    // move branch menu to button
+    moveElToEl(branchMenu, branchButton, 13);
+    
+    branchMenu.classList.remove('top-margin');
+    
+  }
   
 })
 
