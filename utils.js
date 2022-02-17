42230f6b8c6ec072f16c7285703347167d1cb129
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-02-17 22:21:38 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -633,11 +633,6 @@ const repoIntroScreen = `
   </div>
   <div class="subhead">
     <div class="title">Create a repository</div>
-    <div class="subtitle">
-      Click
-      <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 0 24 24" width="16"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z" fill="currentColor"></path></svg>
-      above
-    </div>
   </div>
 </div>
 `;
@@ -649,11 +644,6 @@ const fileIntroScreen = `
   </div>
   <div class="subhead">
     <div class="title">Create a file</div>
-    <div class="subtitle">
-      Click
-      <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 0 24 24" width="16"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z" fill="currentColor"></path></svg>
-      above
-    </div>
   </div>
 </div>
 `;
