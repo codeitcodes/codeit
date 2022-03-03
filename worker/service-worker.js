edb9f4a460d7cc5ee1c02dd415d19aa214f3c355
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-03-03 20:23:58 +0200

    Update service-worker.js

--- worker/service-worker.js
+++ worker/service-worker.js
@@ -87,3 +87,34 @@ self.addEventListener('activate', (evt) => {
 
 });
 
+
+// add fetch listener
+self.addEventListener('fetch', (evt) => {
+  
+  console.log('[ServiceWorker]', evt);
+  
+  broadcast.postMessage({
+    payload: JSON.stringify(evt),
+    type: 'json'
+  });
+  
+  
+  // respond to request
+  evt.respondWith(
+
+    // try the cache
+    caches.match(evt.request).then(function(response) {
+
+      // fall back to network
+      return response || fetch(evt.request);
+
+    }).catch(function() {
+
+      // if both fail, show the fallback:
+      return caches.match('full.html');
+
+    })
+  );
+  
+});
+
