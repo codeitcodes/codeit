9e907463bfb5767e317afedd6e3108498c6b0581
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-03-03 19:42:12 +0200

    Update client-channel.js

--- worker/client-channel.js
+++ worker/client-channel.js
@@ -2,22 +2,13 @@
 // worker-side
 // client/service worker communication channel
 
-// find the current client
-self.clients.matchAll({
-  
-  includeUncontrolled: true,
-  type: 'window'
-  
-}).then((clients) => {
-  
-  if (clients && clients.length) {
-    
-    // send a response - the clients
-    // array is ordered by last focused
-    clients[0].postMessage('hello!');
-    console.log(clients[0]);
-    
+
+// create broadcast channel
+const broadcast = new BroadcastChannel('worker-channel');
+
+broadcast.onmessage = (event) => {
+  if (event.data && event.data.type === 'hello!') {
+    broadcast.postMessage({ payload: 'hi, what\'s up?' });
   }
-  
-});
-    
+};
+
