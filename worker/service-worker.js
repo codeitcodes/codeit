d0ddb4a05f30e440134d09a89d0f3090c8c00174
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-03-03 19:48:50 +0200

    Update service-worker.js

--- worker/service-worker.js
+++ worker/service-worker.js
@@ -5,7 +5,7 @@ self.importScripts('/worker/client-channel.js');
 
 
 // update cache names any time any of the cached files change
-const CACHE_NAME = 'static-cache-v317';
+const CACHE_NAME = 'static-cache-v318';
 
 // list of files to cache
 const FILES_TO_CACHE = [
