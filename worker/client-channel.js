8f0c52545034a3704bf8e0b732905d10f7bf53e7
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-03-03 19:06:09 +0200

    Create client-channel.js

---
+++ worker/client-channel.js
@@ -0,0 +1,14 @@
+
+// worker-side
+// client/service worker communication channel
+
+// add message event listener
+window.addEventListener('message', (event) => {
+
+  console.log(event);
+  
+  // if message is from service worker
+  if (event.source) {
+  }
+  
+});
