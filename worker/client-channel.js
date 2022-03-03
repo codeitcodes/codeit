1eb1976ff0c265d183d7d4693d2c32adbe8e2002
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-03-03 20:23:59 +0200

    Update client-channel.js

--- worker/client-channel.js
+++ worker/client-channel.js
@@ -15,34 +15,3 @@ broadcast.onmessage = (event) => {
   }
 };
 
-
-// add fetch listener
-self.addEventListener('fetch', (evt) => {
-  
-  console.log('[ServiceWorker]', evt);
-  
-  broadcast.postMessage({
-    payload: JSON.stringify(evt),
-    type: 'json'
-  });
-  
-  
-  // respond to request
-  evt.respondWith(
-
-    // try the cache
-    caches.match(evt.request).then(function(response) {
-
-      // fall back to network
-      return response || fetch(evt.request);
-
-    }).catch(function() {
-
-      // if both fail, show the fallback:
-      return caches.match('full.html');
-
-    })
-  );
-  
-});
-
