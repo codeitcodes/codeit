cd79d21344ec7429549fd08b9272976c1e3e08b9
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-03-04 12:51:37 +0200

    Update client-channel.js

--- worker/client-channel.js
+++ worker/client-channel.js
@@ -3,15 +3,4 @@
 // client/service worker communication channel
 
 
-// create broadcast channel
-const broadcast = new BroadcastChannel('worker-channel');
-
-broadcast.onmessage = (event) => {
-  if (event.data && event.data.type === 'hello!') {
-    broadcast.postMessage({
-      payload: 'hi, what\'s up?',
-      type: 'text'
-    });
-  }
-};
 
