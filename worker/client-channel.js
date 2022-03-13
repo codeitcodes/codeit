0e59d5ff5ca2502fe676f790fea0305248ae7a05
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-03-13 22:37:42 +0200

    Update client-channel.js

--- worker/client-channel.js
+++ worker/client-channel.js
@@ -4,7 +4,7 @@
 
 
 // update worker name when updating worker
-const WORKER_NAME = 'codeit-worker-v394';
+const WORKER_NAME = 'codeit-worker-v395';
 
 
 // internal paths
@@ -126,9 +126,7 @@ function handleFetchRequest(request) {
 
     // get request path type
     const pathType = getPathType(request.url);
-    
-    console.log(request);
-    
+        
     // if fetch originated in codeit itself
     if (pathType === 'internal'
         && (getPathType(request.referrer) !== 'run')) {
