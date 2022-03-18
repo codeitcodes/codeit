0abcdf409f2fd24bc6713b1600c74e253165a0df
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-03-18 17:22:34 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -119,6 +119,8 @@ function showMessage(message, duration) {
   // if message is already visible
   if (messageEl.classList.contains('visible')) {
 
+    console.log('a');
+
     // animate new message
 
     messageEl.classList.add('animating');
