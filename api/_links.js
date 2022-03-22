630e3d471ab2c02c20a26aca565ff020a1f1181d
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-03-22 16:42:31 +0200

    Update _links.js

--- api/_links.js
+++ api/_links.js
@@ -1,2 +1,50 @@
 
-
+// decode link
+function decodeLink(url) {
+
+  // save link data
+  let link = new URL(url);
+  let linkData = {};
+
+  if (link.origin === window.location.origin) {
+
+    link = link.searchParams;
+
+
+    if (link.get('dir')) {
+
+      const dir = link.get('dir').split(',');
+
+      if (dir.length === 3) {
+
+        linkData.dir = dir;
+
+
+        if (link.get('file')) {
+
+          const [name, sha] = link.get('file').split(',');
+
+          if (name && sha) {
+
+            linkData.file = {name, sha};
+
+
+            if (link.get('openLive') === 'true') {
+
+              linkData.openLive = true;
+
+            }
+
+          }
+
+        }
+
+      }
+
+    }
+
+  }
+
+  return linkData;
+
+}
