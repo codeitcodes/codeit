4869a6254df08a13f640818100da9f9a64f3ba01
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-03-23 17:26:42 +0200

    Update link.js

---
+++ api/link.js
@@ -0,0 +1,23 @@
+
+export default function handler(request, response) {
+  
+  const query = Object.keys(request.query);
+  
+  let html = '';
+  
+  if (query[0] === 'url') {
+    
+    html = `
+    <oembed>
+      <html><iframe src="https://codeit.codes/api/link?url=`+ query[1] +`&embed=true" width="700" height="480"></html>
+      <width>700</width>
+      <height>480</height>
+    </oembed>
+    `;
+    
+  }
+  
+  response.status(200).send(html);
+  
+}
+
