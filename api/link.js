ab45fae584d59557992d4cf9953ad03c9af93c87
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-03-23 21:53:51 +0200

    Update link.js

--- api/link.js
+++ api/link.js
@@ -96,7 +96,7 @@ const html = `
         
   }
   
-  if (link && !link.startsWith('https:/github.com')) {
+  if (link && link.startsWith('https:/github.com')) {
     
     link = link.replace('https:/github.com', 'https://github.com');
     
