e2f60fec782c52be399f530da9f5a72ae6740c55
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-03-23 22:08:54 +0200

    Update oembed.js

--- api/oembed.js
+++ api/oembed.js
@@ -7,9 +7,15 @@ export default function handler(request, response) {
   
   if (query.url) {
     
+    let url = 'https://codeit.codes';
+    
+    if (query.url.startsWith('https://dev.cde.run/')) {
+      url = 'https://dev.codeit.codes';
+    }
+    
     html = `
     <oembed>
-      <html><iframe src="https://codeit.codes/api/link?url=`+ query.url +`&embed=true" width="700" height="480"></html>
+      <html><iframe src="`+ url +`/api/link?url=`+ query.url +`&embed=true" width="700" height="480"></html>
       <width>700</width>
       <height>480</height>
     </oembed>
