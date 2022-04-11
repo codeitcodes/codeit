8d894c94bdd3078fc2917d54fec1dd8563f46e50
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-04-11 18:19:19 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -464,27 +464,39 @@ let paste = async () => {
 // HTTP Request
 
 let axios = {
-  'get': (url, token) => {
+  'get': (url, token, noParse) => {
     return new Promise((resolve, reject) => {
       try {
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function () {
           if (this.readyState == 4 && String(this.status).startsWith('2')) {
             try {
-              resolve(JSON.parse(this.responseText));
+              if (!noParse) {
+                resolve(JSON.parse(this.responseText));
+              } else {
+                resolve(this.responseText);
+              }
             } catch(e) {
               resolve();
             }
           } else if (this.responseText) {
             try {
-              resolve(JSON.parse(this.responseText));
+              if (!noParse) {
+                resolve(JSON.parse(this.responseText));
+              } else {
+                resolve(this.responseText);
+              }
             } catch(e) {}
           }
         };
         xmlhttp.onerror = function () {
           if (this.responseText) {
             try {
-              resolve(JSON.parse(this.responseText));
+              if (!noParse) {
+                resolve(JSON.parse(this.responseText));
+              } else {
+                resolve(this.responseText);
+              }
             } catch(e) {}
           }
         };
