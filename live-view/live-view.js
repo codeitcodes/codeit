c0f4feecfa1ae288a2133b0a03cbe09fcd186494
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-04-25 17:15:18 +0300

    Update live-view.js

--- live-view/live-view.js
+++ live-view/live-view.js
@@ -64,7 +64,8 @@ async function setupLiveView() {
   
     // if on mobile device
     // and URL has a file
-    if (isMobile && linkData.file && !isEmbed) {
+    if ((isMobile && linkData.file)
+        || isEmbed) {
   
       // close sidebar
       toggleSidebar(false);
