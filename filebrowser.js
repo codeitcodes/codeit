e8eae87b2d6b6320405ecfd2b46843e0d5b08f20
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-04-26 20:17:15 +0300

    Update filebrowser.js

--- filebrowser.js
+++ filebrowser.js
@@ -2266,7 +2266,7 @@ learnShare.addEventListener('click', () => {
     // share invite
     navigator.share({
       title: 'Share Codeit',
-      text: invite
+      url: invite
     });
     
   } else {
