74abb04f2b17a09d342b1bebf3c399c4399106ec
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-04-29 11:46:15 +0300

    Update gitlab-api.js

--- git/gitlab-api.js
+++ git/gitlab-api.js
@@ -1,2 +1,1 @@
-
-
+https://gitlab.com/api/v4/projects?access_token=19a48ef34811b9425b6fc1e4c75f1358bdf7f528d151ec5624c46fa61e8b4c03&order_by=updated_at&min_access_level=30
