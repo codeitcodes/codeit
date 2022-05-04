e4b2138183a6755c728a6aa1a50ec11e7459fbef
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-05-04 16:46:24 +0300

    Update cors.js

--- api/cors.js
+++ api/cors.js
@@ -5,35 +5,27 @@ export default async function handler(request, response) {
   
   const {status, data} = await getRequest(decodeURIComponent(request.query.url));
   
-  response.status(status).json(data);
+  response.status(status).send(data);
   
   
   function getRequest(url) {
   
     return new Promise(resolve => {
       
-      url = new URL(url);
-      
-      const req = https.request({
-        host: url.hostname,
-        path: url.pathname + url.search,
-        method: 'GET',
-        headers: {
-          accept: 'application/json'
-        }
-      }, (resp) => {
+      const req = https.get(url, (resp) => {
         
         let data = '';
         
         resp.on('data', (chunk) => {
+          
           data += chunk;
+          
         });
         
         resp.on('end', () => {
           
-          data = JSON.parse(data);
-          
           resolve({status: resp.statusCode, data: data});
+          
         });
         
       });
