df63a6346fdcea7f1b2d4d3b7ebbf06ccd4073e8
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-05-04 16:33:09 +0300

    Update cors.js

--- api/cors.js
+++ api/cors.js
@@ -1,2 +1,33 @@
 
-
+export default async function handler(request, response) {
+  
+  const https = require('https');
+  
+  const {status, data} = await getRequest(request.query.url);
+  
+  response.status(status).send(data);
+  
+  
+  function getRequest(url) {
+  
+    return new Promise(resolve => {
+      
+      const req = https.get(url, (resp) => {
+        
+        let data = '';
+        
+        resp.on('data', (chunk) => {
+          data += chunk;
+        });
+        
+        resp.on('end', () => {
+          resolve({status: resp.statusCode, data: data});
+        });
+        
+      });
+      
+    });
+    
+  }
+  
+}
