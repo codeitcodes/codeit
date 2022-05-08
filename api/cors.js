2caf58d5dade83896a5483fa81d519095b15ab56
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-05-08 18:51:33 +0300

    Update cors.js

--- api/cors.js
+++ api/cors.js
@@ -3,7 +3,7 @@ export default async function handler(request, response) {
   
   const https = require('https');
   
-  console.log(request.query);
+  console.log(request.query, request.body);
   
   const {status, data} = await getRequest(decodeURIComponent(request.query.url));
   
