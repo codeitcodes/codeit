0a0827fd2f46e78a25f943fe41149fbd7bc06dd4
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-06-08 11:31:11 +0300

    Update oembed.js

--- api/oembed.js
+++ api/oembed.js
@@ -3,7 +3,14 @@ export default function handler(request, response) {
   
   const query = request.query;
   
-  let json = { "error": "Try adding a URL" };
+  const isJSON = query.format ? query.format === 'json' : false;
+  
+  
+  let resp;
+  
+  if (isJSON) resp = { "error": "Try adding a URL" };
+  else resp = 'Try adding a <code>?url=</code>.';
+  
   
   if (query.url) {
     
@@ -13,23 +20,42 @@ export default function handler(request, response) {
       url = 'https://dev.codeit.codes';
     }
     
-    const embedHTML = `<iframe src="`+ url +`/api/link?url=`+ query.url +`&embed=true" width="700" height="480" style="border-radius: 10px;" allow="accelerometer; camera; encrypted-media; display-capture; geolocation; gyroscope; microphone; midi; clipboard-read; clipboard-write" allowfullscreen="true" allowpaymentrequest="true" loading="lazy" sandbox="allow-downloads allow-forms allow-modals allow-pointer-lock allow-popups allow-presentation allow-same-origin allow-scripts allow-top-navigation-by-user-activation" scrolling="yes" frameborder="0"></iframe></html>`;
+    url += '/api/link?url=' + query.url + '&oembed=true';
     
-    json = {
-      "type": "rich",
-      "title": "Codeit",
-      "width": 700,
-      "height": 480,
-      "html": embedHTML,
-      "version": "1.0",
-      "cache_age": 3600,
-      "provider_name": "Codeit",
-      "provider_url": "https://codeit.codes/"
-    };
+    const embedHTML = `<iframe src="`+ url +`" width="700" height="480" style="border-radius: 10px;" allow="accelerometer; camera; encrypted-media; display-capture; geolocation; gyroscope; microphone; midi; clipboard-read; clipboard-write" allowfullscreen="true" allowpaymentrequest="true" loading="lazy" sandbox="allow-downloads allow-forms allow-modals allow-pointer-lock allow-popups allow-presentation allow-same-origin allow-scripts allow-top-navigation-by-user-activation" scrolling="yes" frameborder="0"></iframe></html>`;
+    
+    
+    if (isJSON) {
+    
+      resp = {
+        "type": "rich",
+        "title": "Codeit",
+        "width": 700,
+        "height": 480,
+        "html": embedHTML,
+        "version": "1.0",
+        "cache_age": 3600,
+        "provider_name": "Codeit",
+        "provider_url": "https://codeit.codes/"
+      };
+      
+    } else {
+      
+      resp = `
+      <oembed>
+        <html>`+ embedHTML +`</html>
+        <width>700</width>
+        <height>480</height>
+      </oembed>
+      `;
+      
+    }
     
   }
-    
-  response.status(200).json(json);
+  
+  
+  if (isJSON) response.status(200).json(resp);
+  else response.status(200).html(resp);
   
 }
 
