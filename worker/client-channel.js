56fb0d3f0ace54d50d07f7e327be2d7db8e4b86f
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-06-08 17:18:30 +0300

    Update client-channel.js

--- worker/client-channel.js
+++ worker/client-channel.js
@@ -130,8 +130,8 @@ function sendRequestToClient(request, clientId) {
 
       // if response url matches
       if (event.data.type === 'response' &&
-          event.data.url === url /*&&
-          event.data.fromClient === clientId*/) {
+          event.data.url === url &&
+          event.data.fromClient === clientId) {
 
         if (enableDevLogs) {
           console.debug('[ServiceWorker] Recived response data from client', event.data);
