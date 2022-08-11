db18c2fa7428bf4afb50f3d557e6f38957ce56b6
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-08-11 10:49:19 +0300

    Update context-menu.js

--- context-menu.js
+++ context-menu.js
@@ -1,2 +1,31 @@
 
-
+let contextMenu = {
+  
+  addFileListener: (file) => {
+    
+    if (!isMobile) {
+      
+      file.addEventListener('contextmenu', (e) => {
+        
+        contextMenuEl.classList.add('visible');
+        
+      });
+      
+    }
+      
+  }
+  
+}
+
+
+// disable context menu
+if (!isMobile) {
+
+  window.addEventListener('contextmenu', (e) => {
+
+    e.preventDefault();
+
+  });
+
+}
+
