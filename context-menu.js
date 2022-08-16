93a0e790990c1c48b8faf38475bb5d1de2a59b12
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-08-16 14:57:15 +0300

    Update context-menu.js

--- context-menu.js
+++ context-menu.js
@@ -67,7 +67,7 @@ contextMenu = {
         file.classList.add('active');
         
         onNextFrame(() => {
-          moveElToMouse(contextMenu.el, e, 10);
+          moveElToMouse(contextMenu.el, e, 13);
         });
         
         contextMenu.el.classList.add('visible', 'animating');
