1038eda762d4e6073d5e5e0594d63a34d7172ec5
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-08-23 20:36:03 +0300

    Update codedrop.js

--- codedrop.js
+++ codedrop.js
@@ -125,14 +125,7 @@ function processFile(file) {
 
     cd.history = [{ html: cd.innerHTML, pos: cd.getSelection() }];
 
-    window.addEventListener('load', () => {
-      
-      saveSelectedFileContent();
-      saveSelectedFileCaretPos();
-      saveSelectedFileScrollPos();
-      saveSelectedFileLang();
-      
-    });
+    changeSelectedFile('', '', file.name, event.target.result, cd.lang, [0, 0], [0, 0], false);
 
     showMessage('Loaded ' + file.name + '!', 5000);
 
