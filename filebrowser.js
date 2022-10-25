ed1698e7c7739f13e6b2cabc2ab20b2213639958
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-10-25 14:23:53 +0300

    Update filebrowser.js

--- filebrowser.js
+++ filebrowser.js
@@ -383,7 +383,7 @@ async function renderSidebarHTML() {
     // stop loading
     stopLoading();
     
-    showMessage('Whoops, your Github login expired.', 5000);
+    showMessage('Whoops, your Git login expired.', 5000);
 
     sidebar.classList.add('intro');
 
