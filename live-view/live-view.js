64e567f724025b59a61f57ed9d20b65b8f6acd9d
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-12-18 13:06:43 +0200

    Update live-view.js

--- live-view/live-view.js
+++ live-view/live-view.js
@@ -1119,7 +1119,7 @@ async function renderLiveViewHTML(file) {
   if (!worker.clientId) await worker.installPromise;
 
 
-  liveView.innerHTML = '<iframe src="'+ livePath +'?'+ worker.clientId +'/" name="Live view" title="Live view" class="live-frame" allow="accelerometer; camera; encrypted-media; display-capture; geolocation; gyroscope; microphone; midi; clipboard-read; clipboard-write" allowfullscreen="true" allowpaymentrequest="true" loading="lazy" sandbox="allow-downloads allow-forms allow-modals allow-pointer-lock allow-popups allow-presentation allow-same-origin allow-scripts allow-top-navigation-by-user-activation" scrolling="yes" frameborder="0"></iframe>';
+  liveView.innerHTML = '<iframe src="'+ livePath +'?'+ worker.clientId +'/" name="Live view" title="Live view" class="live-frame" allow="accelerometer; camera; encrypted-media; display-capture; geolocation; gyroscope; microphone; midi; clipboard-read; clipboard-write; web-share; fullscreen; payment" allowfullscreen="true" allowpaymentrequest="true" allowtransparency="true" loading="eager" sandbox="allow-downloads allow-forms allow-modals allow-pointer-lock allow-popups allow-presentation allow-scripts"></iframe>';
 
 
   liveFile = file;
