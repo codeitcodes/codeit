77502dd5fc20914df99da3ce8a71740a12fbe83d
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2023-01-09 12:11:06 +0200

    Update filebrowser.js

--- filebrowser.js
+++ filebrowser.js
@@ -1996,7 +1996,7 @@ function scrolledSidebarTitle() {
 
   }
 
-  if (Math.round(sidebarLogo.offsetWidth + sidebarLogo.scrollLeft)
+  if ((sidebarLogo.offsetWidth + sidebarLogo.scrollLeft - 1)
       >= sidebarLogo.scrollWidth) {
 
     sidebarLogo.classList.add('scrolled-end');
