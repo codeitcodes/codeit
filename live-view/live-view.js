e4094b4010e074e037223a61b7568a1fe5f8ef53
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2023-01-31 12:00:41 +0200

    Update live-view.js

--- live-view/live-view.js
+++ live-view/live-view.js
@@ -652,7 +652,7 @@ if (isMobile) {
         window.scrollTo({left: 0, top: 0, behavior: 'smooth' });
         
       };
-      setTimeout(() => {
+      window.setTimeout(() => {
         window.onscroll = oldScroll;
         
         rect = input.getBoundingClientRect();
