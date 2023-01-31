7be8a4dd189cf860ad5e69170eafe6797f3958fd
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2023-01-31 11:23:46 +0200

    Update live-view.js

--- live-view/live-view.js
+++ live-view/live-view.js
@@ -638,6 +638,14 @@ if (isMobile) {
     // scroll to input on focus in safari
     input.addEventListener('focus', () => {
       
+      const rect = input.getBoundingClientRect();
+        
+      window.scrollTo({
+        top: (rect.top + rect.height - 10.5), // 10.5 is container padding
+        left: 0,
+        behavior: 'smooth'
+      });
+      
       window.setTimeout(() => {
         
         const rect = input.getBoundingClientRect();
