cfe601270632032c6725993b22abcc0e0b85a27d
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2023-02-03 19:17:51 +0200

    Update live-view.js

--- live-view/live-view.js
+++ live-view/live-view.js
@@ -642,9 +642,13 @@ if (isMobile) {
      
       body.style.background = '#0f1014';
       
-      input.prevThemeColor = document.querySelector('meta[name="theme-color"]').content;
-      document.querySelector('meta[name="theme-color"]').content = '#0f1014';
- 
+      onNextFrame(() => {
+  
+        input.prevThemeColor = document.querySelector('meta[name="theme-color"]').content;
+        document.querySelector('meta[name="theme-color"]').content = '#0f1014';
+      
+      });
+      
       /*
       document.querySelector('meta[name="theme-color"]').content = '#313744';
 
