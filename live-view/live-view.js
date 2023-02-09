5fec2181402870789630dd46b726b21df7e41cb8
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2023-02-09 09:47:47 +0200

    Update live-view.js

--- live-view/live-view.js
+++ live-view/live-view.js
@@ -706,6 +706,61 @@ if (isMobile) {
     });
     
   }
+  
+  
+  consoleSheetReturn.addEventListener('click', () => {
+    
+    let codeToRun = input.textContent;
+    
+    logger.log(codeToRun, 'input');
+    
+    codeToRun = codeToRun.replaceAll('`', '\`');
+    
+    const resp = new Function('return eval(`'+ codeToRun +'`)')();
+    
+    logger.log(resp, 'resp');
+    
+  });
+  
+  
+  let logger = {
+    
+    log: (code, type) => {
+      
+      let icon = logger.icons[type];
+      if (!icon) icon = '';
+      
+      const logHTML = `
+      <div class="item">
+        `+ icon +`
+        <cd-el class="code" lang="js">`+ escapeHTML(code) +`</cd-el>
+      </div>
+      `;
+      
+    },
+    
+    icons: {
+      
+        input: `
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="currentColor" class="icon">
+          <path d="M0 0h24v24H0V0z" fill="none"></path>
+          <path d="M9.31 6.71c-.39.39-.39 1.02 0 1.41L13.19 12l-3.88 3.88c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0l4.59-4.59c.39-.39.39-1.02 0-1.41L10.72 6.7c-.38-.38-1.02-.38-1.41.01z"></path>
+        </svg>
+        `,
+        
+        resp: `
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="currentColor" class="icon">
+          <path d="M0 0h24v24H0V0z" fill="none"></path>
+          <g xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+            <path d="M18.29,17.29L18.29,17.29c0.39-0.39,0.39-1.02,0-1.41L14.42,12l3.88-3.88c0.39-0.39,0.39-1.02,0-1.41l0,0 c-0.39-0.39-1.02-0.39-1.41,0l-4.59,4.59c-0.39,0.39-0.39,1.02,0,1.41l4.59,4.59C17.27,17.68,17.9,17.68,18.29,17.29z"></path>
+            <path d="M11.7,17.29L11.7,17.29c0.39-0.39,0.39-1.02,0-1.41L7.83,12l3.88-3.88c0.39-0.39,0.39-1.02,0-1.41l0,0 c-0.39-0.39-1.02-0.39-1.41,0l-4.59,4.59c-0.39,0.39-0.39,1.02,0,1.41l4.59,4.59C10.68,17.68,11.31,17.68,11.7,17.29z"></path>
+          </g>
+        </svg>
+        `
+        
+    }
+    
+  };
 
 } else {
 
