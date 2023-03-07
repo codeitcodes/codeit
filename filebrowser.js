c5cef77d4cce8ce4c6ea5ac648102c1b6a577209
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2023-03-07 15:05:23 +0200

    Update filebrowser.js

--- filebrowser.js
+++ filebrowser.js
@@ -837,7 +837,7 @@ async function renderSidebarHTML(pageNum = 1) {
     fileWrapper.innerHTML += out;
     
     // scroll to end of additional page
-    sidebar.scrollTo(0, sidebar.scrollHeight);
+    sidebar.scrollTo(0, (sidebar.scrollHeight - 1));
     
   }
 
