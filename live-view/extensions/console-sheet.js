7642ca62de0568ce9aa0da6182be6216021b7859
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2023-04-09 22:36:10 +0300

    Update console-sheet.js

--- live-view/extensions/console-sheet.js
+++ live-view/extensions/console-sheet.js
@@ -22,14 +22,14 @@ let consoleSheet = {
     this.el.close.addEventListener('click', () => {
       
       // hide live view console
-      consoleSheet.classList.remove('visible');
+      consoleSheet.el.sheet.classList.remove('visible');
       
     });
     
     this.el.bg.addEventListener('click', () => {
       
       // hide live view console
-      consoleSheet.classList.remove('visible');
+      consoleSheet.el.sheet.classList.remove('visible');
       
     });
     
