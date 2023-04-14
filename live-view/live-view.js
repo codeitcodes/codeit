7564f405003e9940f5fe5cc6af9a6467bcd5d698
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2023-04-14 14:35:22 +0300

    Update live-view.js

--- live-view/live-view.js
+++ live-view/live-view.js
@@ -374,68 +374,6 @@ function addBottomSwipeListener() {
   const draggable = new Draggable(bottomWrapper);
   
   
-  // on click
-  bottomWrapper.addEventListener('click', (e) => {
-    
-    const clickedOnOptions = (e.target === liveButtonOptions);
-    
-    const isExpanded = bottomWrapper.classList.contains('expanded');
-    
-    // if bottom float is expanded
-    if (isExpanded) {
-    
-      // if did not click on options button
-      if (!clickedOnOptions) {
-    
-        e.preventDefault();
-        e.stopPropagation();
-    
-        // retract bottom float
-        bottomWrapper.classList.remove('expanded');
-        
-        // if live view is open, close it
-        if (liveViewToggle) toggleLiveView(selectedFile);
-    
-      }
-    
-    } else { // if bottom float not expanded
-      
-      // if clicked the bottom float's swipe hitbox
-      // but not the bottom float itself
-      if (e.target === bottomWrapper) {
-                
-        // get caret range from point
-        
-        // disable bottom float hitbox
-        bottomWrapper.style.pointerEvents = 'none';
-        
-        const pointX = e.changedTouches[0].clientX,
-              pointY = e.changedTouches[0].clientY;
-        
-        const range = document.caretRangeFromPoint(pointX, pointY);
-        
-        bottomWrapper.style.pointerEvents = '';
-        
-        // if range exists
-        if (range) {
-          
-          e.preventDefault();
-          
-          // select range
-          
-          const sel = window.getSelection();
-          
-          sel.setBaseAndExtent(range.startContainer, range.startOffset, range.endContainer, range.endOffset);
-          
-        }
-        
-      }
-      
-    }
-    
-  });
-  
-  
   // on swipe
   draggable.on('swipe', (e) => {
     
@@ -473,6 +411,68 @@ function addBottomSwipeListener() {
         
       }
     
+    }
+    
+  });
+  
+  
+  // on click
+  bottomWrapper.addEventListener('click', (e) => {
+    
+    const clickedOnOptions = (e.target === liveButtonOptions);
+    
+    const isExpanded = bottomWrapper.classList.contains('expanded');
+    
+    // if bottom float is expanded
+    if (isExpanded) {
+    
+      // if did not click on options button
+      if (!clickedOnOptions) {
+    
+        e.preventDefault();
+        e.stopPropagation();
+    
+        // retract bottom float
+        bottomWrapper.classList.remove('expanded');
+        
+        // if live view is open, close it
+        if (liveViewToggle) toggleLiveView(selectedFile);
+    
+      }
+    
+    } else { // if bottom float not expanded
+      
+      // if clicked the bottom float's swipe hitbox
+      // but not the bottom float itself
+      if (e.target === bottomWrapper) {
+        
+        // get caret range from point
+        
+        // disable bottom float hitbox
+        bottomWrapper.style.pointerEvents = 'none';
+        
+        const pointX = e.clientX,
+              pointY = e.clientY;
+        
+        const range = document.caretRangeFromPoint(pointX, pointY);
+        
+        bottomWrapper.style.pointerEvents = '';
+        
+        // if range exists
+        if (range) {
+          
+          e.preventDefault();
+          
+          // select range
+          
+          const sel = window.getSelection();
+          
+          sel.setBaseAndExtent(range.startContainer, range.startOffset, range.endContainer, range.endOffset);
+          
+        }
+        
+      }
+      
     }
     
   });
