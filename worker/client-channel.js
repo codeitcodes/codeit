257f3b01f42a05c2429492c768ac0b91dfa67907
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2023-04-18 14:01:25 +0300

    Update client-channel.js

--- worker/client-channel.js
+++ worker/client-channel.js
@@ -178,8 +178,6 @@ workerChannel.addEventListener('message', (event) => {
   
 });
 
-if (self.origin === 'https://dev.codeit.codes') self.registration.update();
-
 
 // handle fetch request
 function handleFetchRequest(request, event) {
