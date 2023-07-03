9c5f0133d396ff85bb8e4ea89b3706ef4bd88f72
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2023-07-03 16:27:34 +0300

    Update safari-keyboard.js

--- live-view/extensions/mobile-console/safari-keyboard.js
+++ live-view/extensions/mobile-console/safari-keyboard.js
@@ -98,7 +98,7 @@ class SafariKeyboard {
       
       
       // if blur delta was extremely quick
-      if (blurDelta === this.safariTimeoutDelay) {
+      if (blurDelta < this.safariTimeoutDelay) {
         
         // change wrapper bottom instantly
         this.changeWrapperBottom(this.keyboardHeight);
