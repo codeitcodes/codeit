9ebea8ad589e9f05c8e2b644b0d2e8a70a1e9b88
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2023-08-03 18:53:44 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 
 // update worker name when updating worker
-const WORKER_NAME = 'codeit-worker-v754';
+const WORKER_NAME = 'codeit-worker-v755';
 
 
 self.importScripts('/worker/client-channel.js');
