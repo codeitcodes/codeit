a17bc1279605afe8c030d5f9bce13cef60a85125
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2024-03-11 17:49:28 +0200

    Update gitapi.js

--- git/gitapi.js
+++ git/gitapi.js
@@ -29,7 +29,7 @@ const beforeUnloadListener = (event) => {
 };
 
 
-function ifText(resp) {
+function isText(resp) {
   
   //const response = await fetch('https://example.com/data');
   const arrayBuffer = await resp.arrayBuffer();
